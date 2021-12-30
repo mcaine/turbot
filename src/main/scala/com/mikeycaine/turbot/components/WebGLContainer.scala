d01d268b29cf7f64ce6bc58d3e69fc71c41b4733
Mike Caine <mikey.caine@btinternet.com>
2021-12-30 15:57:39 +0000

    trying to get webglcontainer to work

--- src/main/scala/com/mikeycaine/turbot/components/WebGLContainer.scala
+++ src/main/scala/com/mikeycaine/turbot/components/WebGLContainer.scala
@@ -11,42 +11,53 @@ import scala.scalajs.js
 
 object WebGLContainer {
 
-  case class State(hours: Int, minutes: Int, seconds: Int, elemId: String)
+  case class State(hours: Int, minutes: Int, seconds: Int)
 
-  case class Props(elemId: String, width: Int, height: Int)
+  case class Props(elemId: String)
 
-  class Backend($: BackendScope[Unit, State]) {
+  class Backend($: BackendScope[Props, State]) {
+
     var interval: js.UndefOr[js.timers.SetIntervalHandle] =
       js.undefined
 
     def tick =
-      $.modState(s => State(s.hours, s.minutes, (s.seconds + 1) % 60, s.elemId))
+      $.modState(s => State(s.hours, s.minutes, (s.seconds + 1) % 60))
+
+//    def elementId = CallbackTo ( (p:Props) => {
+//      println("Getting elemId...")
+//      p.elemId
+//    } )
 
     def start = Callback {
+    //def start = elementId >> Callback { targetElementId: String => {
       interval = js.timers.setInterval(1000)(tick.runNow())
 
-      val targetElementId = "yrmum"
+      //val e = elementId.runNow()
+
+      val targetElementId = "xyz"
 
       val width = document.getElementById(targetElementId).clientWidth
       val height = dom.window.innerHeight.toInt
 
-      println(s">> width is $width")
-      println(s">> height is $height")
+      println(s">> width is really $width")
+      println(s">> height is really $height")
 
-          try {
-            World("yrmum", width, height)
-          } catch {
-            case ex: Throwable => println("FullsizeContainer failed in componentDidMount() " + ex)
-          }
+      val w = World(targetElementId, width, height)
+//
+//          try {
+//            //World("yrmum", width, height)
+//          } catch {
+//            case ex: Throwable => println("FullsizeContainer failed in componentDidMount() " + ex)
+//          }
     }
 
-    def clear = Callback {
+    def stop = Callback {
       interval foreach js.timers.clearInterval
       interval = js.undefined
     }
 
-    def render(state: State): VdomTagOf[Div] = {
-      <.div(^.id := state.elemId,
+    def render(state: State, props: Props): VdomElement = {
+      <.div(^.id := props.elemId,
         <.div("Hours: ", state.hours, " Minutes: ", state.minutes, " Seconds: ", state.seconds)
       )
     }
@@ -57,13 +68,17 @@ object WebGLContainer {
 //      .render_P(p => <.div("WEBGLCONTAINER ${p.elemId}"))
 //      .build
 
+  //val world = World("thisxasxs", 1000, 1000)
+
   val Component
-  = ScalaComponent.builder[Unit]
-    .initialState(State(4,20,0, "yrmum"))
-    .renderBackend[Backend]
+  = ScalaComponent.builder[Props]
+    .initialState(State(4,20,0))
+    .backend(new Backend(_))
+    //.renderBackend[Backend]
+    .render_P(props => <.div(^.id := props.elemId))
     .componentDidMount(_.backend.start)
-    .componentWillUnmount(_.backend.clear)
+    .componentWillUnmount(_.backend.stop)
     .build
 
-  def apply(elemId: String) = Component()
+  def apply(elemId: String) = Component(Props(elemId))
 }
